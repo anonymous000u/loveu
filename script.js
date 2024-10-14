bbfc4f9ea6bc67e80bfaebb36a0e4044f2dd6540
anonymous000u <tininhogaqw@gmail.com>
2024-10-13 23:16:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let btn_no = document.getElementById("btn_no")
+let btn_yes = document.getElementById("btn_yes")
+let block_p = document.getElementById("block_p")
+let block_audio = document.getElementById("block_audio")
+let audio = document.getElementById("audio")
+let h1 = document.getElementById("h1")
+
+btn_no.addEventListener("click", function(){
+    btn_no.innerHTML = "<b>YES</b>"
+    block_audio.style.display = "flex" ;
+    h1.style.marginTop = "100px";
+})
+
+btn_yes.addEventListener("click", function(){
+    block_audio.style.display = "flex" ;
+    h1.style.marginTop = "100px";
+})
+
+
+audio.addEventListener("play", function(){
+    block_p.style.display = "flex" ;
+})
